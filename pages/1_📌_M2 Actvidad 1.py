a00d2bc4a6dbbac31196f7a2b15f2990cc863a8e
lina marin j <linamarin2011@gmail.com>
2025-04-11 18:43:36 -0500

    Avance en actividades 1 y 2

--- pages/1_📌_M2 Actvidad 1.py
+++ pages/1_📌_M2 Actvidad 1.py
@@ -1,29 +1,102 @@
 import streamlit as st
+import pandas as pd
 
 # Configuración de la página
-st.set_page_config(   
-    page_icon="📌",
-    layout="wide"
-)
+st.set_page_config(page_icon="📌", layout="wide")
 
 st.title("Momento 2 - Actividad 1")
 
-st.header("Descripción de la actividad")
-st.markdown("""
+st.header("Creación de DataFrames en Pandas con Streamlit")
+st.markdown(
+    """
 Esta actividad es una introducción práctica a Python y a las estructuras de datos básicas.
 En ella, exploraremos los conceptos fundamentales de Python y aprenderemos a utilizar variables,
 tipos de datos, operadores, y las estructuras de datos más utilizadas como listas, tuplas,
 diccionarios y conjuntos.
-""")
+"""
+)
 
 st.header("Objetivos de aprendizaje")
 
-st.markdown("""
+st.markdown(
+    """
 - Comprender los tipos de datos básicos en Python
 - Aprender a utilizar variables y operadores
 - Dominar las estructuras de datos fundamentales
 - Aplicar estos conocimientos en ejemplos prácticos
-""")
+"""
+)
 
 st.header("Solución")
 
+
+biblioteca = {
+    "Titulo": [
+        "orgullo y perjuicio",
+        "orgullo y perjuicio",
+        "orgullo y perjuicio",
+        "orgullo y perjuicio",
+    ],
+    "Autor": ["juanita", "juanita", "juanita", "juanita"],
+    "Año de publicación": ["1998", "1998", "1998", "1998"],
+    "Genero": ["Romance", "Romance", "Romance", "Romance"],
+}
+
+df_bibioteca = pd.DataFrame(biblioteca)
+
+
+st.subheader("DataFrame de libros")
+st.write(
+    """1. Crea un diccionario en tu script con al menos cuatro claves: "título", "autor", año de publicación" y "género". Para cada clave asigna una lista con datos de ejemplo sobre libros (por ejemplo, 3 o 4 libros distintos)."""
+)
+st.code(biblioteca)
+st.dataframe(df_bibioteca)
+
+lista_biblioteca = [
+    {
+        "Titulo": "orgullo y perjuicio",
+        "Autor": "juanita",
+        "Año de publicación": "1998",
+        "Genero": "Romance",
+    },
+    {
+        "Titulo": "orgullo y perjuicio",
+        "Autor": "juanita",
+        "Año de publicación": "1998",
+        "Genero": "Romance",
+    },
+    {
+        "Titulo": "orgullo y perjuicio",
+        "Autor": "juanita",
+        "Año de publicación": "1998",
+        "Genero": "Romance",
+    },
+    {
+        "Titulo": "orgullo y perjuicio",
+        "Autor": "juanita",
+        "Año de publicación": "1998",
+        "Genero": "Romance",
+    },
+]
+
+df_lista_biblioteca = pd.DataFrame(lista_biblioteca)
+
+
+st.write("DataFrame de libros")
+st.code(biblioteca)
+st.dataframe(df_lista_biblioteca)
+
+
+# listas de listas
+
+productos = [
+    ["Pantalón", 100.000, 15],
+    ["Vestido", 55.000, 50],
+    ["Zapatos", 120.00, 80],
+    ["Camisa", 80.000, 30],
+]
+
+columnas = ["Producto", "Precio", "Cantidad en Stock"]
+df_productos = pd.DataFrame(productos, columns=columnas)
+
+st.dataframe(df_productos)
--- pages/2_📌_M2 Actvidad 2.py
+++ pages/2_📌_M2 Actvidad 2.py
@@ -1,29 +1,88 @@
+import os
+import pandas as pd
 import streamlit as st
 
+
 # Configuración de la página
-st.set_page_config(   
-    page_icon="📌",
-    layout="wide"
-)
+st.set_page_config(page_icon="📌", layout="wide")
 
 st.title("Momento 2 - Actividad 2")
 
-st.header("Descripción de la actividad")
-st.markdown("""
-Esta actividad es una introducción práctica a Python y a las estructuras de datos básicas.
-En ella, exploraremos los conceptos fundamentales de Python y aprenderemos a utilizar variables,
-tipos de datos, operadores, y las estructuras de datos más utilizadas como listas, tuplas,
-diccionarios y conjuntos.
-""")
+st.header("Estudiantes en Colombia")
+st.markdown(
+    """
+En esta actividad, se trabajará con un archivo CSV que contiene información de 50 estudiantes en Colombia,
+ incluyendo datos como su nombre, edad, ciudad, promedio académico y asistencia. 
+"""
+)
 
-st.header("Objetivos de aprendizaje")
+st.header("Objetivo de aprendizaje")
 
-st.markdown("""
-- Comprender los tipos de datos básicos en Python
-- Aprender a utilizar variables y operadores
-- Dominar las estructuras de datos fundamentales
-- Aplicar estos conocimientos en ejemplos prácticos
-""")
+st.markdown(
+    """
+    El objetivo principal es desarrollar una aplicación interactiva con Streamlit que permita explorar 
+    y visualizar fácilmente la informacion información del archivo CSV.
+
+"""
+)
 
 st.header("Solución")
 
+
+st.title("🧑‍🎓 Bienvenido al explorador de Estudiantes")
+
+st.subheader("¿Qué deseas hacer hoy con los registros?")
+
+opcion = st.selectbox(
+    "Elige una opción:",
+    (
+        "Ver primeras y últimas 5 filas",
+        "Ver el resumen con .info() y .describe().)",
+        "Seleccionar columnas específicas",
+        "Filtrar por promedio",
+    ),
+)
+
+
+archivo = pd.read_csv("estudiantes_colombia.csv")
+
+# Opción 1
+if opcion == "Ver primeras y últimas 5 filas":
+    # primeras 5  filas
+
+    st.subheader("▪️ Primeras 5 filas")
+    st.dataframe(archivo.head())
+
+    # ultimas  5 filas
+    st.subheader("▪️ Últimas 5 filas")
+    st.dataframe(archivo.tail())
+
+
+# Opción 2
+elif opcion == "Resumen del dataset (.info y .describe)":
+    st.subheader("Resumen general del dataset")
+    # mostrar resumen
+    st.write(archivo.info())
+
+    st.text("Resumen estadístico con .describe():")
+    # Mostrar estadistica
+    st.write(archivo.describe())
+
+
+# Opción 3: Selección de columnas
+elif opcion == "Seleccionar columnas específicas":
+    st.subheader("Que columnas quieres ver")
+    columnas = st.multiselect(
+        "Selecciona una o más columnas:",
+        archivo.columns.tolist(),
+        default=["nombre", "edad", "ciudad", "promedio", "asistencia"],
+    )
+    st.dataframe(archivo[columnas])
+
+
+# Opción 4: # Filtrar estudiantes con promedio mayor a un valor definido por el usuario (usando un slider).
+
+elif opcion == "Filtrar por promedio":
+    st.subheader("Filtra estudiantes según su promedio")
+    st.subheader("promedio mayor de 3")
+    st.write(archivo[archivo["promedio"] > 3])
